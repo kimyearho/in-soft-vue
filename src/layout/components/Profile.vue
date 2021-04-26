a35aef7983d0d60850ade9f2585eea362677a9ea
yeonho <yeonho@in-soft.co.kr>
2021-04-26 19:16:56 +0900

    프로필 스타일 수정

--- src/layout/components/Profile.vue
+++ src/layout/components/Profile.vue
@@ -1,51 +1,55 @@
 <template>
-  <v-container
-    :style="{width: '50px'}"
-  >
-    <v-row justify="center">
-      <v-menu
-        class="profile_container"
-        bottom
-        min-width="200px"
-        rounded
-        offset-y
-      >
-        <template v-slot:activator="{on}">
-          <v-btn
-            icon
-            v-on="on"
-          >
-            <v-avatar
-              color="red"
-              size="42"
+  <v-container>
+    <v-row class="profile">
+      <v-col cols="6">
+        <p>Kim</p>
+        <p>Yeonho</p>
+      </v-col>
+      <v-col cols="6">
+        <v-menu
+          class="profile_container"
+          bottom
+          min-width="200px"
+          rounded
+          offset-y
+        >
+          <template v-slot:activator="{on}">
+            <v-btn
+              icon
+              v-on="on"
             >
-              <span class="white--text headline">{{ user.initials }}</span>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-list-item-content class="justify-center">
-            <div class="mx-auto text-center">
-              <v-avatar color="brown">
+              <v-avatar
+                color="red"
+                size="42"
+              >
                 <span class="white--text headline">{{ user.initials }}</span>
               </v-avatar>
-              <h3>{{ getUser.name }}</h3>
-              <p class="caption mt-1">
-                {{ getUser.email }}
-              </p>
-              <v-divider class="my-3" />
-              <v-btn
-                depressed
-                rounded
-                text
-                @click="logout"
-              >
-                Logout
-              </v-btn>
-            </div>
-          </v-list-item-content>
-        </v-card>
-      </v-menu>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-list-item-content class="justify-center">
+              <div class="mx-auto text-center">
+                <v-avatar color="brown">
+                  <span class="white--text headline">{{ user.initials }}</span>
+                </v-avatar>
+                <h3>{{ getUser.name }}</h3>
+                <p class="caption mt-1">
+                  {{ getUser.email }}
+                </p>
+                <v-divider class="my-3" />
+                <v-btn
+                  depressed
+                  rounded
+                  text
+                  @click="logout"
+                >
+                  Logout
+                </v-btn>
+              </div>
+            </v-list-item-content>
+          </v-card>
+        </v-menu>
+      </v-col>
     </v-row>
   </v-container>
 </template>
